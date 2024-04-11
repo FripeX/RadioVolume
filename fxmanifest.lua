7caec084e246d23cf1a433aa9dc3a7b7777d01ff
FripeX <61631931+FripeX@users.noreply.github.com>
2024-04-11 16:38:15 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Frip_Scripts'
+description 'Volume Radio Key'
+version '1.0'
+
+lua54 'yes'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	'cl_volradio/*.lua'
+}
+
+dependencies {
+	'es_extended'
+}
